ece79bfdaffc84399bc007f94b72ac5d6c7fddcb
filipi <filipisee@hotmail.com>
2021-09-27 20:47:07 -0300

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 // Style
 import styled from "styled-components";
 // Material UI Icons
-import SearchIcon from '@mui/icons-material/Search';
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 
 const Container = styled.div`
   height: 60px;
@@ -11,6 +12,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
@@ -18,7 +20,9 @@ const Wrapper = styled.div`
 
 const Left = styled.div`
   flex: 1;
-  background-color: #ccc;
+  display: flex;
+  background-color: #f78;
+  align-items: center;
 `;
 
 const Language = styled.span`
@@ -26,20 +30,40 @@ const Language = styled.span`
   cursor: pointer;
 `;
 
-//const SearchContainer = styled
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
 
 // Center Div
 
 const Center = styled.div`
   flex: 1;
-  background-color: #bbb;
+  text-align: center;
+  background-color: #f43;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 
 // Right Div
 
 const Right = styled.div`
   flex: 1;
-  background-color: #aaa;
+  background-color: #f47;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
 `;
 
 const Navbar = () => {
@@ -48,11 +72,24 @@ const Navbar = () => {
       <Container>
         <Wrapper>
           <Left>
-              <Language>EN</Language>
-              <SearchIcon/>
+            <Language>EN</Language>
+            <SearchContainer>
+              <Input />
+              <Search />
+            </SearchContainer>
           </Left>
-          <Center>Center</Center>
-          <Right>Right</Right>
+          <Center>
+            <Logo>E-COMMERCE APP</Logo>
+          </Center>
+          <Right>
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+              <Badge badgeContent={100} color="secondary">
+                <ShoppingCartOutlined />
+              </Badge>
+            </MenuItem>
+          </Right>
         </Wrapper>
       </Container>
     </>
