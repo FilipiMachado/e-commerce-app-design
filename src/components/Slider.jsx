ce100931110ca396e3090b49ee20de6ab79ad664
filipi <filipisee@hotmail.com>
2021-09-28 20:37:55 -0300

    slider updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,16 +1,17 @@
+import React, { useState } from "react";
 // Style
 import styled from "styled-components";
 // Material UI Icons
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
-// Images
-import Woman from "../assets/woman.png";
+// Slider items
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
-  background-color: lightseagreen;
+  overflow: hidden;
 `;
 
 // Slider Div
@@ -31,10 +32,13 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -42,6 +46,7 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: ${(props) => props.bg};
 `;
 
 const ImgContainer = styled.div`
@@ -77,56 +82,40 @@ const Button = styled.button`
   font-size: 20px;
   cursor: pointer;
   border-radius: 15px;
-  color: #008080;
   background-color: #fff;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
     <>
       <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={() => handleClick("left")}>
           <ArrowLeftOutlined />
         </Arrow>
-        <Wrapper>
-          <Slide>
-            <ImgContainer>
-              <Image src={Woman} />
-            </ImgContainer>
-            <InfoContainer>
-              <Title>SUMMER SALE</Title>
-              <Desc>
-                DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
-              </Desc>
-              <Button>SHOW NOW</Button>
-            </InfoContainer>
-          </Slide>
-          <Slide>
-            <ImgContainer>
-              <Image src={Woman} />
-            </ImgContainer>
-            <InfoContainer>
-              <Title>SUMMER SALE</Title>
-              <Desc>
-                DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
-              </Desc>
-              <Button>SHOW NOW</Button>
-            </InfoContainer>
-          </Slide>
-          <Slide>
-            <ImgContainer>
-              <Image src={Woman} />
-            </ImgContainer>
-            <InfoContainer>
-              <Title>SUMMER SALE</Title>
-              <Desc>
-                DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
-              </Desc>
-              <Button>SHOW NOW</Button>
-            </InfoContainer>
-          </Slide>
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map((item) => (
+            <Slide bg={item.bg}>
+              <ImgContainer>
+                <Image src={item.img} />
+              </ImgContainer>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOW NOW</Button>
+              </InfoContainer>
+            </Slide>
+          ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={() => handleClick("right")}>
           <ArrowRightOutlined />
         </Arrow>
       </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1623288516140-47a0a17cec7c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=680&q=80",
+        title: "MODA VERÃO",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#D5B686",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1613966570650-add3cf83aa83?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
+        title: "MODA PRIMAVERA",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#D18599",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1485043621645-c983f19e23c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        title: "MODA POPULAR",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "#9EBFC8",
+    }
+]
