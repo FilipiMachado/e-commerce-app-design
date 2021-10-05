7ace79fa4f6aaba3aecdd416f1bf11bfcc4a7fc4
filipi <filipisee@hotmail.com>
2021-10-05 15:38:00 -0300

    register updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,41 +10,67 @@ const Container = styled.div`
     url("https://images.unsplash.com/photo-1570915226741-cc7d678ad7ce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=728&q=80")
       center;
 
-      display: flex;
-      align-items: center;
-      justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: #fff;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 500;
+`;
 
-const Form = styled.form``;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
 
-const Agreement = styled.span``;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #00a5a5;
+  color: white;
+  font-weight: 700;
+  cursor: pointer;
+`;
 
 const Register = () => {
   return (
     <>
       <Container>
         <Wrapper>
-          <Title>CREATE AN ACCOUNT</Title>
+          <Title>CRIE UMA CONTA</Title>
           <Form>
-            <Input placeholder="name" />
-            <Input placeholder="last name" />
-            <Input placeholder="username" />
+            <Input placeholder="nome" />
+            <Input placeholder="sobrenome" />
+            <Input placeholder="usuário" />
             <Input placeholder="email" />
-            <Input placeholder="password" />
-            <Input placeholder="confirm password" />
+            <Input placeholder="senha" />
+            <Input placeholder="confirme a senha" />
             <Agreement>
               Ao criar uma conta, eu concordo com os termos de nossa{" "}
               <b>POLÍTICA DE PRIVACIDADE</b>.
             </Agreement>
-            <Button>CREATE</Button>
+            <Button>CRIAR</Button>
           </Form>
         </Wrapper>
       </Container>
