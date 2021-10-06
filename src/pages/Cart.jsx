a31aa84c44bfe4ef4fd9430fd3c8022ccb6bff19
filipi <filipisee@hotmail.com>
2021-10-05 23:02:37 -0300

    cart updated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,6 +4,8 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 // Style
 import styled from "styled-components";
+// Icons
+import { Add, Remove } from "@material-ui/icons";
 
 const Container = styled.div``;
 
@@ -54,15 +56,63 @@ const Info = styled.div`
   flex: 3;
 `;
 
-const Product = styled.div``;
-const ProductDetail = styled.div``;
-const Image = styled.img``;
-const Details = styled.div``;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
 const ProductName = styled.span``;
+
 const ProductId = styled.span``;
-const ProductColor = styled.div``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
 const ProductSize = styled.span``;
-const PriceDetail = styled.div``
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 26px;
+  font-weight: 400;
+`;
 
 const Summary = styled.div`
   flex: 1;
@@ -88,7 +138,7 @@ const Cart = () => {
             <Info>
               <Product>
                 <ProductDetail>
-                  <Image src="" />
+                  <Image src="https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
                   <Details>
                     <ProductName>
                       <b>Product:</b> JESSIE THUNDER SHOES
@@ -96,13 +146,20 @@ const Cart = () => {
                     <ProductId>
                       <b>ID:</b> 93813718292
                     </ProductId>
-                    <ProductColor />
+                    <ProductColor color="black" />
                     <ProductSize>
                       <b>Size:</b> 34
                     </ProductSize>
                   </Details>
                 </ProductDetail>
-                <PriceDetail>price</PriceDetail>
+                <PriceDetail>
+                  <ProductAmountContainer>
+                    <Remove />
+                    <ProductAmount>2</ProductAmount>
+                    <Add />
+                  </ProductAmountContainer>
+                  <ProductPrice>$ 30</ProductPrice>
+                </PriceDetail>
               </Product>
             </Info>
             <Summary>summary</Summary>
